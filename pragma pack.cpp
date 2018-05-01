3c15b3d810a1242204598eb24abf39d1e3e66594
Priyam Roy <priyamroy137@gmail.com>
2018-05-01 20:57:01 +0530

    Add files via upload

---
+++ pragma pack.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#pragma pack(1)
+struct emp 
+{
+	int a;
+	char b;
+	float c;
+};
+#pragma pack()
+
+int main()
+{
+	struct emp e;
+	printf("%u %u %u",&e.a,&e.b,&e.c);
+	return 0;
+}
